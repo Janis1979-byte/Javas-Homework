5a1efcfc673c2107d26e2c67b9d2f9ab2436b78d
Janis1979-byte <janis.ruselis@inbox.lv>
2021-02-27 10:13:11 +0200

    2 mājas darbs

--- src/homework2/CheckTheStateOfXy.java
+++ src/homework2/CheckTheStateOfXy.java
@@ -11,11 +11,11 @@ public class CheckTheStateOfXy {
         int ySize = scanner.nextInt();
         scanner.close();
         if ((xSize == -2 && ySize == 1) || (xSize == 3 && ySize == 1) || (xSize == 3 && ySize == -4) || (xSize == -2 && ySize == -4)) {
-            System.out.println("Koordinātas " + "(" + (xSize) + ";" + (ySize) + ")" + "atrodas uz figūras līnijas!");
+            System.out.println("Koordinātas " + "(" + (xSize) + ";" + (ySize) + ")" + " atrodas uz figūras līnijas!");
         } else if (xSize < -2 || xSize > 3 || ySize < -4 || ySize > 1) {
-            System.out.println("Koordinātas " + "(" + (xSize) + ";" + (ySize) + ")" + "atrodas figūras ārpusē!");
+            System.out.println("Koordinātas " + "(" + (xSize) + ";" + (ySize) + ")" + " atrodas figūras ārpusē!");
         } else {
-            System.out.println("Koordinātas " + "(" + (xSize) + ";" + (ySize) + ")" + "atrodas figūras iekšienē!");
+            System.out.println("Koordinātas " + "(" + (xSize) + ";" + (ySize) + ")" + " atrodas figūras iekšienē!");
         }
     }
 }
