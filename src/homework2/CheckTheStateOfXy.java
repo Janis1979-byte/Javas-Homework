f30004a3c7451dd72e9c769d5eaf0d081386b2b9
Janis1979-byte <janis.ruselis@inbox.lv>
2021-03-01 11:39:50 +0200

    2 mājas darbs

--- src/homework2/CheckTheStateOfXy.java
+++ src/homework2/CheckTheStateOfXy.java
@@ -10,15 +10,15 @@ public class CheckTheStateOfXy {
         System.out.print("Lūdzu ievadīt Y koordināti: ");
         int ySize = scanner.nextInt();
         scanner.close();
-        if ((xSize == -2 && ySize == 1) || (xSize == 3 && ySize == 1) || (xSize == 3 && ySize == -4) || (xSize == -2 && ySize == -4)) {
-            System.out.println(String.format("Koordinātas (%d;%d) atrodas uz figūras līnijas!", xSize, ySize));
-        } else if (xSize < -2 || xSize > 3 || ySize < -4 || ySize > 1) {
-            System.out.println(String.format("Koordinātas (%d;%d) atrodas figūras ārpusē!", xSize, ySize));
-        } else {
+        if (xSize > -2 && xSize < 3 && ySize < 1 && ySize > -4) {
             System.out.println(String.format("Koordinātas (%d;%d) atrodas figūras iekšienē!", xSize, ySize));
-        }
+        } else if (xSize <= 3 && xSize >= -2 && ySize >= -4 && ySize <= 1) {
+            System.out.println(String.format("Koordinātas (%d;%d) atrodas uz figūras līnijas!", xSize, ySize));
+        } else
+            System.out.println(String.format("Koordinātas (%d;%d) atrodas figūras ārpusē!", xSize, ySize));
     }
 }
+
 
 
 
