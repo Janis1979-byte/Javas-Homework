efd04c36ebaa508ab02cbe5de3648ba2eb00b187
Janis1979-byte <janis.ruselis@inbox.lv>
2021-03-06 00:57:53 +0200

    3 mājas darbs

---
+++ src/homework3/HelloUser.java
@@ -0,0 +1,16 @@
+package homework3;
+
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        System.out.print("Ievadiet vārdu, uzvārdu: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        String surname = scanner.next();
+        scanner.close();
+        System.out.printf("'%s'\n", name.substring(0, 1).toUpperCase() + name.substring(1));
+        System.out.printf("'%s'", surname.toUpperCase());
+
+    }
+}
